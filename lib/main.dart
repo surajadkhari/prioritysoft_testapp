f235c1b90381d64a8865b4e7951a02bf1eb8bfb3
“Suat” <surajadkhari@gmail.com>
2024-05-28 11:48:55 +1000

    initalize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:priorityshoft_testapp/app/app.dart';
+import 'package:priorityshoft_testapp/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ShoeApp());
 }
