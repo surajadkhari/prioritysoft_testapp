f5ffb2e8ceb6855d4d150bac1fecdf6d94501808
“Suat” <surajadkhari@gmail.com>
2024-05-28 15:38:40 +1000

    category section

--- lib/features/home/presentation/home.dart
+++ lib/features/home/presentation/home.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:priorityshoft_testapp/const/app_color_const.dart';
 import 'package:priorityshoft_testapp/const/app_string_const.dart';
+
+final categoryProvider = StateProvider<int>((ref) {
+  return 0;
+});
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
@@ -16,34 +23,47 @@ class DiscoverScreen extends StatelessWidget {
         centerTitle: false,
         shadowColor: Colors.transparent,
         surfaceTintColor: Colors.white,
-        title: Text(
-          AppStringResource.khomeAppbarTitle,
-          style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            AppStringResource.khomeAppbarTitle,
+            style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+          ),
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const CategoryHeading(),
-            Expanded(
-              child: GridView.builder(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 0.0, horizontal: .0),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 0.8,
-                    crossAxisSpacing: 10,
-                    crossAxisCount: 2),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const Center(child: HomeProductCart());
-                },
-              ),
+            CategoryHeading(
+              onPressed: () {},
             ),
+            const HomeProductList(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class HomeProductList extends StatelessWidget {
+  const HomeProductList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: .0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 0.8, crossAxisSpacing: 10, crossAxisCount: 2),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Center(child: HomeProductCart());
+        },
       ),
     );
   }
@@ -115,11 +135,12 @@ class HomeProductCart extends StatelessWidget {
   }
 }
 
-class CategoryHeading extends StatelessWidget {
-  const CategoryHeading({super.key});
-
+class CategoryHeading extends ConsumerWidget {
+  const CategoryHeading({super.key, required this.onPressed});
+  final void Function()? onPressed;
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(categoryProvider);
     List<String> categories = [
       "All",
       "Nike",
@@ -131,41 +152,20 @@ class CategoryHeading extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          ...categories.map((e) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(e),
-              )),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("ALl"),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("Nike"),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("ALl"),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("Nike"),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("ALl"),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.symmetric(horizontal: 20),
-          //   child: Text("Nike"),
-          // ),
-
-          // ...categories.map(
-          //   (e) => Text(
-          //     e,
-          //     style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          //   ),
-          // ),
+          ...categories.map((e) => TextButton(
+              onPressed: () {
+                ref.read(categoryProvider.notifier).state =
+                    categories.indexOf(e);
+              },
+              child: Text(
+                e,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: value == categories.indexOf(e)
+                      ? const Color(0xff0D0B0B)
+                      : const Color(0xffB7B7B7),
+                ),
+              ))),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:priorityshoft_testapp/app/app.dart';
 import 'package:priorityshoft_testapp/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ShoeApp());
+  runApp(const ProviderScope(child: ShoeApp()));
 }
