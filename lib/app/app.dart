e3e11b336735df69b9c3859b1a615c2a9211c24d
“Suat” <surajadkhari@gmail.com>
2024-05-28 12:04:29 +1000

    discover screen and folder structure

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:priorityshoft_testapp/features/home/presentation/home.dart';
 
 class ShoeApp extends StatelessWidget {
   const ShoeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: DiscoverScreen(),
+    );
   }
 }
